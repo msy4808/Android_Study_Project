9ec452b78c3a1c89f2eeae72a6f714fe88730c22
문 성 운 <mkik90911@naver.com>
2022-01-18 21:49:02 +0900

    새 엑티비티 추가 및 화면전환 기능 구현

--- app/src/main/java/android/text/org/MainActivity.java
+++ app/src/main/java/android/text/org/MainActivity.java
@@ -1,8 +1,11 @@
 package android.text.org;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButtonClicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+    public void onButtonClicked2(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-0000-1111"));
+        startActivity(myIntent);
+    }
+    public void onButtonClicked3(View v) {
+        Intent myIntent = new Intent(getApplicationContext(), NewActivity.class);
+        startActivity(myIntent);
     }
 }
